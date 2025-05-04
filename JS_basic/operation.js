2c933ff140b6addd8793ce3c47f92f593203ff01
Prathmesh Chauhan <0231cse088@niet.co.in>
2025-05-04 18:28:28 +0000

    done

---
+++ JS_basic/operation.js
@@ -0,0 +1,45 @@
+let num0=3;
+let num1=-num0;
+console.log(num1); // -3
+
+console.log("raj"+" suman"); // raj suman
+
+console.log("raj"+2 , 9+"raj"); // raj2 9raj
+
+console.log(3+10+"kali"+8+5); // 13kali85
+
+console.log(4+7); // 11
+console.log(4-7); // -3
+console.log(4*3); // 12
+console.log(4**3); // 64
+console.log((-5)**3); // -125
+console.log(8%3); // 2
+console.log(16/3); // 5.333333333333333
+console.log(19.5*4) // 78
+console.log(19.5*4.0); //78
+console.log(78/4); //19.5
+console.log(78.875%4.88); // 0.7950000000000017
+
+let a,b,c;
+a=b=c=5;
+console.log(a,b,c) // 5 5 5
+
+// seeing POST_INCREMENT & PRE_INCREMENT :
+
+let x1,y1,x2,y2;
+
+x1=6;
+y1=x1++;
+console.log("\nx1 = ",x1,"y1 = ",y1,"\n"); // x1=7 y1=6
+
+x2=9;
+y2=++x2;
+console.log("x2 = ",x2,"y2 = ",y2); // x2=10 y2=10
+
+
+
+
+
+
+
+

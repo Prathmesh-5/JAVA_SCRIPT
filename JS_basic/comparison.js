53838704cfe4ed33f1f660a4a272deb58c2da652
Prathmesh Chauhan <0231cse088@niet.co.in>
2025-05-04 18:46:31 +0000

    container config done

---
+++ JS_basic/comparison.js
@@ -0,0 +1,10 @@
+console.log(2>1); // true
+console.log(2<1); // false
+console.log(6.0==6); // true
+console.log(6.0>6); // false
+console.log(6.0<6); // false
+console.log(7.8211<7.821); // false
+console.log(7.8211>7.821); // true
+console.log(7!=7); // false
+
+
